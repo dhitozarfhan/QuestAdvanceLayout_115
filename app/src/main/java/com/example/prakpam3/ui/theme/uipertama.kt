4a424e455ccf2785a3e217de9ca3b41868c6160c
dhitozarfhan <dhitozarfhan@gmail.com>
2025-10-23 16:27:21 +0700

    Menambahkan komponen layout Compose

--- app/src/main/java/com/example/prakpam3/ui/theme/uipertama.kt
+++ app/src/main/java/com/example/prakpam3/ui/theme/uipertama.kt
@@ -2,3 +2,6 @@ package com.example.prakpam3
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
