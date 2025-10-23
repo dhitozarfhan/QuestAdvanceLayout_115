d3f947a4ef2ae5c4575519c500ebac5ae2967e89
dhitozarfhan <dhitozarfhan@gmail.com>
2025-10-23 16:28:06 +0700

    menampilkan teks, gambar

--- app/src/main/java/com/example/prakpam3/ui/theme/uipertama.kt
+++ app/src/main/java/com/example/prakpam3/ui/theme/uipertama.kt
@@ -5,3 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
